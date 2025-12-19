5374feaf41189f7388a7e43616ba617e5caecb43
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-12-19 13:29:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let alreadyTriggered = false;
 /* ===================== */
 /* 🎥 AUTOPLAY SAFE */
 /* ===================== */
-mainVideo.src = 'video/Video1.mp4';
+mainVideo.src = 'video/Video1.MOV';
 mainVideo.muted = true;          // OBLIGATOIRE
 mainVideo.playsInline = true;
 mainVideo.preload = 'auto';
