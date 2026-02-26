3baadea272c125eb3d7c25f9498a7202caaae33f
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-02-26 19:36:40 +0100

    Remove unnecessary closing bracket in menu.js

--- menu.js
+++ menu.js
@@ -40,7 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
     sessionStorage.removeItem("unlockMessage");
   }
 
-});
 /* ==========================================================
    🌌 MÉTÉO
 ========================================================== */
