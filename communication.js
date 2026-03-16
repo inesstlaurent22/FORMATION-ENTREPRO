d4f767727dab3c289dd2423684f805cb500c21b6
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-03-16 22:27:03 +0100

    Update communication.js

--- communication.js
+++ communication.js
@@ -505,7 +505,9 @@ r.className = "mg3-column mg3-right";
 ].forEach(t=>{
 
   const b=document.createElement("button");
-  b.className="mg3-btn-right1";
+
+  b.className="mg3-btn mg3-btn-right1";
+
   b.textContent=t[0];
 
   b.onclick=()=>{
