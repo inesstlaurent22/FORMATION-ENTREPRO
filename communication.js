a213abb39151e9ea1a873c905ba730a5de9fd30b
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-03-16 22:06:27 +0100

    Update communication.js

--- communication.js
+++ communication.js
@@ -482,7 +482,7 @@ const l = document.createElement("div");
 l.className = "mg3-column mg3-left";
 
 const r = document.createElement("div");
-r.className = "mg3-column mg3-right1";
+r.className = "mg3-column mg3-right";
 
   let sel=null, ok=0;
 
