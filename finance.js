cf1a159a050d8a0c4a4f0523079d5212c0bcd813
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-03-18 23:45:59 +0100

    Update finance.js

--- finance.js
+++ finance.js
@@ -261,7 +261,7 @@ const answers = shuffleArray([
   ...questions[qIndex].bad.map(t => ({ t, ok: false }))
 ]);
 
-    minigame0.forEach(a => {
+      answers.forEach(a => {
       const btn = document.createElement("button");
       btn.textContent = a.t;
       btn.onclick = () => {
